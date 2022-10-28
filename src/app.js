a50bf9894d332d3d41cc451e3cab9fe59102ae80
Esavwede <esavwede84@gmail.com>
2022-10-28 13:37:30 -0700

    added env

--- src/app.js
+++ src/app.js
@@ -1,12 +1,11 @@
 const express = require('express') 
-
 const app = express() 
 const routes = require('./routes/index.route')
 
 
 routes(app) 
 
-const PORT = 5000 
+const PORT = process.env.PORT 
 app.listen(PORT, ()=>{
     console.log(' Application Listening On Port ' + PORT )
 })
